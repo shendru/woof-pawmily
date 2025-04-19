0e57b2be339e23678a0f7801388c8f3c286fecd6
Andrew(Shendru) <dev.andrewearl@gmail.com>
2025-04-19 20:59:15 +0800

    Added TailwindCSS

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <div>Image</div>
+        <div>Navbars</div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
