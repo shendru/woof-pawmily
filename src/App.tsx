5f1395e98f60b2eb6ead10946107f151022d6072
Andrew(Shendru) <dev.andrewearl@gmail.com>
2025-04-19 22:09:37 +0800

    Added Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
+import Header from "./components/header/Header.tsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
+      <Header />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,36 @@
 import React from "react";
+import logo from "../../assets/images/logo.png";
+import "./header.css";
 
 function Header() {
   return (
-    <header>
-      <nav>
-        <div>Image</div>
-        <div>Navbars</div>
-      </nav>
+    <header className="">
+      <div className="p-2">
+        <a href="/" className="flex items-center justify-center p-2 mb-2">
+          <img src={logo} alt="" className="w-[18%]" />
+        </a>
+        <nav>
+          <div className="md:w-[80%] md:mx-auto sm:w-[100%]">
+            <div className="flex justify-center sm:w-full xl:w-[80%] py-1 mx-auto border-b-4 border-t border-l border-r border-[#E98842] rounded-lg gap-5 text-[1.2rem] font-bold text-[#14192C]">
+              <a className="nav-links px-5" href="">
+                HOME
+              </a>
+              <a className="nav-links px-5" href="">
+                PAWPEDIA
+              </a>
+              <a className="nav-links px-5" href="">
+                PRODUCTS
+              </a>
+              <a className="nav-links px-5" href="">
+                ABOUT
+              </a>
+              <a className="nav-links px-5" href="">
+                CONTACT
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
     </header>
   );
 }
