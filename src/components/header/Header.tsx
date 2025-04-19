b10b8b728bc01cd80486c70fec8696d654f6d62c
Andrew(Shendru) <dev.andrewearl@gmail.com>
2025-04-19 22:21:13 +0800

    Updated Header.tsx | Added Hover FX on nav-links

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -11,20 +11,40 @@ function Header() {
         </a>
         <nav>
           <div className="md:w-[80%] md:mx-auto sm:w-[100%]">
-            <div className="flex justify-center sm:w-full xl:w-[80%] py-1 mx-auto border-b-4 border-t border-l border-r border-[#E98842] rounded-lg gap-5 text-[1.2rem] font-bold text-[#14192C]">
-              <a className="nav-links px-5" href="">
+            <div className="flex justify-center sm:w-full xl:w-[80%] mx-auto border-b-4 border-t border-l border-r border-[#E98842] rounded-lg gap-5 text-[1.2rem] font-bold text-[#14192C] text-center">
+              <a
+                className="nav-links px-5 py-1 hover:text-[#E98842] hover:bg-[rgba(255,255,255,0.5)]"
+                href=""
+                style={{ width: "190px" }}
+              >
                 HOME
               </a>
-              <a className="nav-links px-5" href="">
+              <a
+                className="nav-links px-5 py-1 hover:text-[#E98842] hover:bg-[rgba(255,255,255,0.5)]"
+                href=""
+                style={{ width: "190px" }}
+              >
                 PAWPEDIA
               </a>
-              <a className="nav-links px-5" href="">
+              <a
+                className="nav-links px-5 py-1 hover:text-[#E98842] hover:bg-[rgba(255,255,255,0.5)]"
+                href=""
+                style={{ width: "190px" }}
+              >
                 PRODUCTS
               </a>
-              <a className="nav-links px-5" href="">
+              <a
+                className="nav-links px-5 py-1 hover:text-[#E98842] hover:bg-[rgba(255,255,255,0.5)]"
+                href=""
+                style={{ width: "190px" }}
+              >
                 ABOUT
               </a>
-              <a className="nav-links px-5" href="">
+              <a
+                className="nav-links px-5 py-1 hover:text-[#E98842] hover:bg-[rgba(255,255,255,0.5)]"
+                href=""
+                style={{ width: "190px" }}
+              >
                 CONTACT
               </a>
             </div>
